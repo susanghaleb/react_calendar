d5bf2d25170ef4c46fc527989e7f9a2b9e7770be
power-puff <susanaghaleb@gmail.com>
2020-10-06 16:58:29 -0300

    feat: update component Weekday, inject reminders and mapper with component Reminder

--- src/components/weekday/weekday.stories.js
+++ src/components/weekday/weekday.stories.js
@@ -12,4 +12,10 @@ const Template = (args) => <Weekday {...args} />
 export const WeekdayComponent = Template.bind({})
 WeekdayComponent.args = {
   label: 1,
+  reminders: [
+    { label: 'Buy pet food', color: 'red', id: 'kjnsdv' },
+    { label: 'Go to the mechanic', id: 'kjnsdvcwcew' },
+    { label: 'Do exercises!!', id: 'wefewgfeawg' },
+    { label: 'Pay services', color: 'pink', id: 'ewfgewageagaer' },
+  ],
 }
