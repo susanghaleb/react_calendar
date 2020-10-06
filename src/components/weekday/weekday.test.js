2b964b481769cf7cb6693adcf39e80af0bd3cd75
power-puff <susanaghaleb@gmail.com>
2020-10-06 12:19:58 -0300

    review: normalized renaming description test

--- src/components/weekday/weekday.test.js
+++ src/components/weekday/weekday.test.js
@@ -3,25 +3,25 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { Weekday } from './weekday.component'
 
-test('load and display day one', () => {
+test('load and display weekday 1st', () => {
   const { container } = render(<Weekday label={1} />)
   expect(screen.getByText('1')).toHaveTextContent(1)
   expect(container.firstChild).toHaveClass('square')
 })
 
-test('load and display day one disabled', () => {
+test('load and display weekday 1st disabled', () => {
   const { container } = render(<Weekday label={1} disabled />)
   expect(screen.getByText('1')).toHaveTextContent(1)
   expect(container.firstChild).toHaveClass('square square--disabled')
 })
 
-test('load and display day one weekend', () => {
+test('load and display weekday 1st weekend', () => {
   const { container } = render(<Weekday label={1} isWeekend />)
   expect(screen.getByText('1')).toHaveTextContent(1)
   expect(container.firstChild).toHaveClass('square square--weekend')
 })
 
-test('load and display day one weekend and disabled', () => {
+test('load and display weekday 1st weekend and disabled', () => {
   const { container } = render(<Weekday label={1} isWeekend disabled />)
   expect(screen.getByText('1')).toHaveTextContent(1)
   expect(container.firstChild).toHaveClass(
