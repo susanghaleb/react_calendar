510385346ebf688b879b1604c337d80f25f331cd
power-puff <susanaghaleb@gmail.com>
2020-10-06 17:02:38 -0300

    chore: update story weekday, add weekday component with reminders

--- src/components/weekday/weekday.stories.js
+++ src/components/weekday/weekday.stories.js
@@ -12,6 +12,12 @@ const Template = (args) => <Weekday {...args} />
 export const WeekdayComponent = Template.bind({})
 WeekdayComponent.args = {
   label: 1,
+  reminders: [],
+}
+
+export const WeekdayWithReminders = Template.bind({})
+WeekdayWithReminders.args = {
+  label: 1,
   reminders: [
     { label: 'Buy pet food', color: 'red', id: 'kjnsdv' },
     { label: 'Go to the mechanic', id: 'kjnsdvcwcew' },
